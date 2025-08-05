c08d7221574f05fe65738b72c8242782a035daaf
alninja <albert.ningwa@gmail.com>
2025-08-05 20:04:17 +0300

    Adjusted the conversion formular

--- converter.py
+++ converter.py
@@ -5,4 +5,4 @@ def celcius_to_fahrenheit(celsius_temp):
     This function converts a temperature from celcius to 
     Fahrenheit
     """
-    return (celsius_temp/90 +32)
+    return (celsius_temp* 9/5 +32)
